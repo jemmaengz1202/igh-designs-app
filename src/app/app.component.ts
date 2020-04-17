f9b1f4210780737d93c62e14b9123649dcfd48af
jemmaengz <juanemmanuel1202@gmail.com>
2020-04-16 22:46:02 -0500

    Cambios en app.component.ts para FCM
    Cambia el componente app para agregar notificaciones por medio del
    módulo FCM de firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { FCM } from '@ionic-native/fcm';
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,12 +12,45 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+           platform    : Platform,
+           statusBar   : StatusBar,
+           splashScreen: SplashScreen,
+           fcm         : FCM,
+    public alertCtrl   : AlertController)
+  {
     platform.ready().then(() => {
+      //Notifications
+      fcm.subscribeToTopic('all');
+      fcm.getToken().then(token=>{
+          console.log(token);
+      });
+      fcm.onNotification().subscribe(data=>{
+        // if(data.wasTapped){
+        //   console.log("Received in background");
+        // } else {
+        //   console.log("Received in foreground");
+        // };
+        this.showAlert('Alerta:', data.text);
+      });
+      fcm.onTokenRefresh().subscribe(token=>{
+        console.log(token);
+      });
+      
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  private showAlert(_title: string, text: string) {
+    const alert = this.alertCtrl.create({
+      title   : _title,
+      subTitle: text,
+      cssClass: 'custom-alert-danger',
+      buttons : ['Cerrar']
+    });
+    alert.present();
   }
 }
