6c97d6a3d1999a99d1f4c3fcec9a347c1c73443e
jemmaengz <juanemmanuel1202@gmail.com>
2020-04-16 22:56:18 -0500

    Agrega firebase function para notificaciones push
    Agrega firebase function que envía notificaciones push cuando un
    registro es generado en /alerta en firebase realtime database.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,62 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createNewNode = 
+functions.database
+    .ref('/alertas/{alertaId}')
+    .onCreate((snapshot, context) => {
+        const _date = Date.now();
+        
+        return snapshot.ref.parent.parent.child('lista').push({
+            text: snapshot.val(),
+            date: _date
+        });
+    });
+
+
+exports.pushNotificationAlerta = 
+functions.database
+    .ref('/alertas/{alertaId}')
+    .onCreate((snapshot, context)  => {
+        const value = snapshot.val();
+        console.log(`Valor obtenido: ${value}`);
+
+        const payload = {
+            notification: {
+                title: "Alerta",
+                body : value,
+            },
+            android: {
+                notification: {
+                    sound       : "default",
+                    click_action: "FCM_PLUGIN_ACTIVITY"
+                }
+            },
+            data: {
+                text: value
+            },
+            topic: "all",
+        };
+
+        const options = {
+            priority  : "high",
+            timeToLive: 60 * 60 * 24
+        };
+
+        return admin.messaging().send(payload).then(res => {
+            console.log("Successfully sent message:", res)
+        }).catch(err => {
+            console.log("Error sending message:", err);
+
+        })
+
+    });
