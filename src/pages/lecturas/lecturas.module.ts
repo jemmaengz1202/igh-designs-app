098056ee9c03ebd29ccb2fb99e5b48898cf05e29
jemmaengz <juanemmanuel1202@gmail.com>
2020-04-16 22:49:16 -0500

    Agrega la panalla Lecturas de la aplicación
    Agrega la pantalla Lecturas, que incluye:
    * lecturas.html
    * lecturas.module.ts
    * lecturas.scss
    * lecturas.ts

---
+++ src/pages/lecturas/lecturas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LecturasPage } from './lecturas';
+
+@NgModule({
+  declarations: [
+    LecturasPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LecturasPage),
+  ],
+})
+export class LecturasPageModule {}
---
+++ src/pages/lecturas/lecturas.ts
@@ -0,0 +1,35 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LecturasPage page.
+ *
+ * See https:   //ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector   : 'page-lecturas',
+  templateUrl: 'lecturas.html',
+})
+export class LecturasPage {
+  temperatura: any;
+  agua1      : any;
+  agua2      : any;
+
+  constructor(
+    public navCtrl  : NavController,
+    public navParams: NavParams,
+    public db       : AngularFireDatabase) {
+      this.temperatura = db.object('temperatura').valueChanges();
+      this.agua1       = db.object('agua1').valueChanges();
+      this.agua2       = db.object('agua2').valueChanges();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LecturasPage');
+  }
+
+}
