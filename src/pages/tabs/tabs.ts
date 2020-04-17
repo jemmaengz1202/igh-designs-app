f6a0537db7ac8ca7a06896da95081c67e4fbe5c5
Juan Enríquez <jemmaengz1202@gmail.com>
2020-04-16 22:53:39 -0500

    Agrega la página Lecturas a tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,3 +1,4 @@
+import { LecturasPage } from './../lecturas/lecturas';
 import { ConexionPage } from './../conexion/conexion';
 import { AlertasPage } from './../alertas/alertas';
 import { Component } from '@angular/core';
@@ -11,6 +12,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AlertasPage;
   tab3Root = ConexionPage;
+  tab4Root = LecturasPage;
 
   constructor() {
 
